43ec429bed668cd31e81467374326d1430a3a8c3
scolerad134 <gugac134@gmail.com>
2024-11-28 16:00:43 +0300

    init project

---
+++ src/main/java/com/romashkaco/RomashkacoAppApplication.java
@@ -0,0 +1,13 @@
+package com.romashkaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkacoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkacoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romashkaco/RomashkacoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romashkaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkacoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
